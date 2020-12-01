ca7c7b86100b7d9dbe17234f48ea817e3b8a326f
Iago Martínez <iago@fundapps.co>
2020-12-01 18:58:19 +0000

    Second star found (inelegantly)

--- aoc2020/day1.py
+++ aoc2020/day1.py
@@ -11,8 +11,15 @@ def main():
             x = nums[ix]
             for iy in range(ix + 1, len(nums)):
                 if nums[ix] + nums[iy] == 2020:
-                    print(f'Found it! {nums[ix]} * {nums[iy]} = {nums[ix] * nums[iy]}')
+                    print(f'Found the two! {nums[ix]} * {nums[iy]} = {nums[ix] * nums[iy]}')
                     break
+
+        for ix in range(0, len(nums)):
+            for iy in range(ix + 1, len(nums)):
+                for iz in range(iy + 1, len(nums)):
+                    if nums[ix] + nums[iy] + nums[iz] == 2020:
+                        print(f'Found the three! {nums[ix]} * {nums[iy]} * {nums[iz]} = {nums[ix] * nums[iy] * nums[iz]}')
+                        break
 
 if __name__ == '__main__':
     sys.exit(main())
