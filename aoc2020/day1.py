3e7f0e1f07da69917afa1204b9764c7ca06f6a82
Iago Martínez <iago@fundapps.co>
2020-12-01 18:54:24 +0000

    Naïve solution to Day 1

--- aoc2020/day1.py
+++ aoc2020/day1.py
@@ -7,6 +7,12 @@ def main():
         for line in f:
             nums.append(int(line))
         
+        for ix in range(0, len(nums)):
+            x = nums[ix]
+            for iy in range(ix + 1, len(nums)):
+                if nums[ix] + nums[iy] == 2020:
+                    print(f'Found it! {nums[ix]} * {nums[iy]} = {nums[ix] * nums[iy]}')
+                    break
 
 if __name__ == '__main__':
     sys.exit(main())
