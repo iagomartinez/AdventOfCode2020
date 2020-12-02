e6dae3a17fa2450751608ed80b8be86725299062
Iago Martínez <iago@fundapps.co>
2020-12-02 13:21:37 +0000

    Happy case: min chars are found = valid password

--- aoc2020/day2.py
+++ aoc2020/day2.py
@@ -9,11 +9,17 @@ class ValidationTests(unittest.TestCase):
         parsed = parse('2-8 t: pncmjxlvckfbtrjh')
         self.assertTupleEqual(parsed, (2,8,'t','pncmjxlvckfbtrjh'))
 
+    def test_is_valid_min_char(self):
+        self.assertTrue(is_valid(2, 't', 'tt'))
+
 def parse(line):
     policy, password = line.split(': ')
     range, char = policy.split(' ')
     min,max = range.split('-')
     return (int(min),int(max),char,password)
+
+def is_valid(min, char, password):
+    return password.count(char) >= min
 
 def main():
     with open('../_data/day2.txt', 'r',newline='', encoding='utf-8') as f:
