fff64a50b3f14e889b3e8dd3e190aff253f5f7d5
Iago Martínez <iago@fundapps.co>
2020-12-06 10:35:15 +0000

    Decode complete

--- aoc2020/day5.py
+++ aoc2020/day5.py
@@ -2,20 +2,28 @@ import sys
 import unittest
 
 class Day5Tests(unittest.TestCase):
-    def test_decoderow(self):
-        row, _ = decode('FBFBBFFRLR')
-        self.assertEquals(row, 44)
+    def test_decodeseat(self):
+        row, col = decode('FBFBBFFRLR')
+        self.assertEquals((44,5), (row,col))
 
-    def test_getrowcode(self):
+    def test_decoderow(self):
         rowcode = 'FBFBBFFRLR'[0:7]
         self.assertEqual(7, len(rowcode))
         rowcode = rowcode.replace('F', '0').replace('B', '1')
         self.assertEqual('0101100', rowcode)
 
+    def test_decodecol(self):
+        colcode = 'FBFBBFFRLR'[7:11]
+        self.assertEqual(3, len(colcode))
+        colcode = colcode.replace('R','1').replace('L','0')
+        self.assertEqual('101', colcode)
+
 def decode(seatcode):
     rowcode = seatcode[0:7]
-    rowcode = rowcode.replace('F', '0').replace('B', '1')    
-    return int(rowcode, 2), None
+    rowcode = rowcode.replace('F', '0').replace('B', '1')
+    colcode = seatcode[7:11]
+    colcode = colcode.replace('R','1').replace('L','0')
+    return int(rowcode, 2), int(colcode, 2)
 
 def main():
     print('Day 5')
