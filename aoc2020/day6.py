e8ce310dc4340a84224798f7b307b6dab39b206d
Iago Martínez <iago@fundapps.co>
2020-12-08 00:17:32 +0000

    Day 6 Part 1 Done

--- aoc2020/day6.py
+++ aoc2020/day6.py
@@ -5,26 +5,36 @@ from collections import Counter
 class Day6Tests(unittest.TestCase):
     def test_counter(self):
         group = Counter('abc')
-        self.assertEquals(group['a'], 1)
+        self.assertEqual(group['a'], 1)
     
     def test_sample(self):
-        with open('../_data/day6_sample.txt', 'r', newline='', encoding='utf-8') as f:
-            groups = [Counter()]
-            g = 0
-            for line in f:                
-                answers = line.rstrip()
-                print(answers)
-                if answers == '':
-                    g += 1
-                    groups.append(Counter())
-                    print(len(groups))
-                else:
-                    groups[g] += Counter(answers)
-        print(groups)
-        self.assertEquals(sum([len(g) for g in groups]), 11)                            
+        groups = groupanswers('../_data/day6_sample.txt')
+        self.assertEqual(sum([len(g) for g in groups]), 11) 
+
+    def test_countfullfilegroups(self):                           
+        groups = groupanswers('../_data/day6.txt')
+        self.assertEqual(len(groups), 504)
+
+def groupanswers(file, verbose=False):
+    with open(file, 'r', newline='', encoding='utf-8') as f:
+        groups = [Counter()]
+        g = 0
+        for line in f:                
+            answers = line.rstrip()
+            if answers == '':
+                g += 1
+                groups.append(Counter())
+            else:
+                groups[g] += Counter(answers)
+    if verbose:
+        print(groups[0])
+        print(groups[-1])
+    return groups
 
 def main():    
-    print('Day 6')
+    print('####### Day 6 #########')
+    groups = groupanswers('../_data/day6.txt', True)
+    print(sum([len(g) for g in groups]))
 
 if __name__ == '__main__':
     sys.exit(main())
