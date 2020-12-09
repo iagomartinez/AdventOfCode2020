a1be52130ee6971258e70f5ba73d2d3d459d03cf
Iago Martínez <iago@fundapps.co>
2020-12-09 23:48:10 +0000

    Day 8 Part 1 done!

--- aoc2020/day8.py
+++ aoc2020/day8.py
@@ -7,9 +7,14 @@ class AOCTests(unittest.TestCase):
         self.assertEqual(('nop', 0), parse('nop +0'))
         self.assertEqual(('acc', -99), parse('acc -99'))
     
-    def test_parsesample(self):
+    def test_sampleprogram(self):
         commands = parsefile('../_data/day8_sample.txt')
         self.assertEqual(9, len(commands))
+        rt = BootCodeRunTime(commands, True)
+        with self.assertRaises(InfiniteLoopError) as il:
+            for _ in rt.next():
+                pass
+        self.assertEqual(5, il.exception.acc)
 
     def test_commands(self):
         rt = BootCodeRunTime([parse('nop +0'), parse('acc +99'), parse('jmp +2'), parse('nop +0'), parse('acc -57')], True)
@@ -25,7 +30,7 @@ class BootCodeRunTime():
         self.stack = []
         self.pointer = 0
         self.v = verbose
-        self.symbol = {'nop':self.__nop, 'acc': self.__acc, 'jmp': self.__jmp}
+        self.execute = {'nop':self.__nop, 'acc': self.__acc, 'jmp': self.__jmp}
 
     def __nop(self, arg):
         return 1
@@ -40,13 +45,19 @@ class BootCodeRunTime():
     def next(self):
         while self.pointer < len(self.program):
             cmd, arg = self.program[self.pointer]
-            increment = self.symbol[cmd](arg)
+            if (self.pointer, cmd, arg) in self.stack:
+                raise InfiniteLoopError(self.acc)
+            increment = self.execute[cmd](arg)
             self.stack.append((self.pointer, cmd, arg))
             self.pointer += increment
             if self.v:
                 print(f'cmd: {cmd}, arg: {arg}, acc: {self.acc}, next: {self.pointer}')
             yield self.acc
-    
+
+class InfiniteLoopError(Exception):
+    def __init__(self, acc):
+        self.acc = acc
+
 def parse(line, verbose=False):
     r = re.compile('(?P<cmd>(nop|acc|jmp))\s(?P<arg>[\+\-]{1}\d{1,})') 
     m = r.match(line)
@@ -60,7 +71,13 @@ def parsefile(file, verbose=False):
         return [parse(line.strip(), verbose) for line in f]
 
 def main():
-    print('Day 8')
+    print('------- Day 8 ---------')
+    rt = BootCodeRunTime(parsefile('../_data/day8.txt'), True)
+    try:
+        for _ in rt.next():
+            pass
+    except InfiniteLoopError as il:
+        print(f'Infinite loop detected! acc: {il.acc}')
 
 if __name__ == '__main__':
     sys.exit(main())
