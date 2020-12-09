56e608c43dd008d6b5e34aef6ce8f396326a8e6e
Iago Martínez <iago@fundapps.co>
2020-12-09 23:15:09 +0000

    Instructions work, now for the infinite loop

--- aoc2020/day8.py
+++ aoc2020/day8.py
@@ -2,14 +2,52 @@ import sys
 import unittest
 import re
 
-class Day8Tests(unittest.TestCase):
+class AOCTests(unittest.TestCase):
     def test_parse(self):
         self.assertEqual(('nop', 0), parse('nop +0'))
         self.assertEqual(('acc', -99), parse('acc -99'))
     
     def test_parsesample(self):
-        parsefile('../_data/day8_sample.txt', True)
+        commands = parsefile('../_data/day8_sample.txt')
+        self.assertEqual(9, len(commands))
 
+    def test_commands(self):
+        rt = BootCodeRunTime([parse('nop +0'), parse('acc +99'), parse('jmp +2'), parse('nop +0'), parse('acc -57')], True)
+        for acc in rt.next():
+            print(acc)
+        self.assertEqual(42, rt.acc)
+        self.assertEqual([('nop', 0), ('acc', 99), ('jmp', 2), ('acc', -57)], rt.stack)
+
+
+class BootCodeRunTime():
+    def __init__(self, program, verbose=False):
+        self.program = program
+        self.acc = 0
+        self.stack = []
+        self.pointer = 0
+        self.v = verbose
+        self.map = {'nop':self.__nop, 'acc': self.__acc, 'jmp': self.__jmp}
+
+    def __nop(self, arg):
+        self.pointer += 1
+        return None
+    
+    def __acc(self, arg):
+        self.acc += arg
+        self.pointer += 1
+
+    def __jmp(self, arg):
+        self.pointer += arg
+
+    def next(self):
+        while self.pointer < len(self.program):
+            cmd, arg = self.program[self.pointer]
+            self.map[cmd](arg)
+            self.stack.append((cmd, arg))
+            if self.v:
+                print(f'cmd: {cmd}, arg: {arg}, pointer: {self.pointer}')
+            yield self.acc
+    
 def parse(line, verbose=False):
     r = re.compile('(?P<cmd>(nop|acc|jmp))\s(?P<arg>[\+\-]{1}\d{1,})') 
     m = r.match(line)
