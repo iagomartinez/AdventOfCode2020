178d97db293ca86fc76c491405f1dfe3b151b0e1
Iago Martínez <iago@fundapps.co>
2020-12-09 15:13:24 +0000

    Got the recursion working, but resulting set contains dupes - i think it still works?

--- aoc2020/day7.py
+++ aoc2020/day7.py
@@ -12,9 +12,56 @@ class Day7Tests(unittest.TestCase):
         self.assertEqual([['faded blue', None]], parsed)
 
     def test_sample(self):
-        with open('../_data/day7_sample.txt', 'r', newline='', encoding='utf-8') as f:
-            rules = [r for line in f for r in parse(line, True) ]        
+        rules = parsefile('../_data/day7_sample.txt')      
         self.assertEqual(len(rules), 15)
+        paths = find('shiny gold', rules, [])
+        paths = sorted(paths, key=len)#print(paths)
+        for p in paths:
+            print( p)
+        self.assertEqual(len(paths), 4)        
+
+    def test_simplecase(self):
+        rules =  parsefile('../_data/day7_simplecase.txt')
+        self.assertEqual(len(rules), 5)
+        paths = find('shiny gold', rules, [])
+        paths = sorted(paths, key=len)#print(paths)
+        for p in paths:
+            print( p)
+        self.assertEqual(len(paths), 2)
+
+
+def find(colour, rules, acc):
+    containers = [r for r in rules if r[-1] == colour]
+    if not containers:
+        print(f'base case: {colour}, acc:{acc}')
+        paths = []
+        for p in acc:
+            ext = [colour]
+            ext.extend(p)
+            paths.append(ext)       
+        return paths
+    
+    paths = []
+    #print(f'outer recursive case: acc {acc}, colour: {colour}, containers:{containers}, rules:{rules}')
+    for c in containers:
+        #print(f'inner recursive case: acc {acc}, colour: {colour}, container:{c}')
+        if not acc:
+            print(f'recursive case: not acc, colour: {colour}')
+            paths += find(c[0], [r for r in rules if not r[-1] == colour], [c])
+        else:
+            print(f'recursive case: acc {acc}, colour: {colour}')
+            for p in acc:
+                ext = [colour]
+                ext.extend(p)
+                paths.append(p)
+            paths += find(c[0], [r for r in rules if not r[-1] == colour], paths)
+    print(f'recursion return: paths:{paths}')
+    return paths
+
+def parsefile(file, verbose = False):
+    with open(file, 'r', newline='', encoding='utf-8') as f:
+        rules = [r for line in f for r in parse(line, verbose)]
+    return rules        
 
 def parse(line, verbose = False):
     subject, contents = line.split('bags contain')
