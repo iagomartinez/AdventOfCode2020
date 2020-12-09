3e89b1a1020fc0c0147e3ed388cb5418c806baf7
Iago Martínez <iago@fundapps.co>
2020-12-09 21:13:29 +0000

    First working version of countbags

--- aoc2020/day7.py
+++ aoc2020/day7.py
@@ -7,15 +7,15 @@ from toposort import toposort, toposort_flatten
 class Day7Tests(unittest.TestCase):
     def test_parse(self):
         parsed = parse('light red bags contain 1 bright white bag, 2 muted yellow bags.')
-        self.assertEqual([['light red', 'bright white'], ['light red', 'muted yellow']], parsed)
+        self.assertEqual([('light red', 'bright white', 1), ('light red', 'muted yellow', 2)], parsed)
     
     def test_parsecontainsnone(self):
         parsed = parse('faded blue bags contain no other bags.')
-        self.assertEqual([['faded blue', None]], parsed)
+        self.assertEqual([('faded blue', None, 0)], parsed)
 
     def test_parsewithcounts(self):
         parsed = parse('light red bags contain 1 bright white bag, 2 muted yellow bags.')
-        self.assertEqual([['light red', 'bright white'], ['light red', 'muted yellow']], parsed)
+        self.assertEqual([('light red', 'bright white', 1), ('light red', 'muted yellow',2)], parsed)
     
     def test_rulestonodes(self):
         rules =  parsefile('../_data/day7.txt')
@@ -25,21 +25,40 @@ class Day7Tests(unittest.TestCase):
 
     def test_graphwalk(self):
         rules =  parsefile('../_data/day7_sample.txt')
-        nodes = nodesfrom(rules, True)
+        nodes = nodesfrom(rules)
         containers = walkup('light red', nodes)
         self.assertFalse(containers)
         containers = walkup('bright white', nodes)
         self.assertEqual({'light red', 'dark orange'}, containers)
         containers = walkup('shiny gold', nodes)
-        self.assertEqual({'light red', 'bright white', 'muted yellow', 'dark orange'}, containers)   
+        self.assertEqual({'light red', 'bright white', 'muted yellow', 'dark orange'}, containers)
 
-    # def test_countbags(self):
-    #     rules =  parsefile('../_data/day7_sample.txt')
-    #     nodes = nodesfrom(rules, True)
+    def test_countbags(self):
+        rules =  parsefile('../_data/day7_sample.txt')
+        nodes = nodesfrom(rules)
+        self.assertEqual(0, countbags('faded blue', nodes))
+        self.assertEqual(5 + 6, countbags('vibrant plum', nodes, True))
+        self.assertEqual(32, countbags('shiny gold', nodes, True))
 
-        
+
+    def test_findtuple(self):
+        l = {('a', 1), ('b', 2)}
+        self.assertTrue('a' in [c for c,_ in l])
+
+def countbags(colour, nodes, verbose=False):
+    if nodes[colour] == {(None, 0)}:
+        return 0
+    
+    bags = 0
+    contained = [n for n in nodes[colour]]
+    for bag, count in contained:
+        if verbose:
+            print(f'bag: {bag}, count: {count}')
+        bags += count + count * countbags(bag, nodes)
+    return bags
+
 def walkup(colour, nodes):
-    containers = [k for k,v in nodes.items() if colour in v]
+    containers = [k for k,val in nodes.items() if colour in [c for c,_ in val]]
     if not containers:
         return set()
     
@@ -50,11 +69,11 @@ def walkup(colour, nodes):
 
 def nodesfrom(rules, verbose=False):
     nodes = dict()
-    for r in rules:
-        if r[0] not in nodes:
-            nodes[r[0]] = {r[-1]}
+    for l,r,num in rules:
+        if l not in nodes:
+            nodes[l] = {(r, num)}
         else:
-            nodes[r[0]].add(r[-1])
+            nodes[l].add((r, num))
     if verbose:
         for n,v in nodes.items():
             print(f'{n}: {v}')
@@ -68,13 +87,13 @@ def parsefile(file, verbose = False):
 def parse(line, verbose = False):
     subject, contents = line.split('bags contain')
     if 'no other bags' in contents:
-        c = [[subject.strip(), None]]
+        c = [(subject.strip(), None, 0)]
         if verbose:
             print(f'{line} -> {c}')
-        return [[subject.strip(), None]]
+        return c
 
-    matches = re.findall('\d{1,}\s(?P<colour>.*?)\sbag(?:s?)', contents)
-    colours = [[subject.strip(), c.strip()] for c in matches]
+    r = re.compile('(?P<bagcount>\d{1,})\s(?P<colour>.*?)\sbag(?:s?)')
+    colours = [(subject.strip(), m['colour'].strip(), int(m['bagcount'])) for m in r.finditer(contents)]
     if verbose:
         print(f'{line} -> {colours}')
     return colours
@@ -82,7 +101,6 @@ def parse(line, verbose = False):
 def main():
     rules =  parsefile('../_data/day7.txt')
     containers = walkup('shiny gold',nodesfrom(rules))
-    #paths = sorted(paths, key=len)
     print(f'bags that can contain shiny gold: {len(containers)}')
 
 if __name__ == '__main__':
