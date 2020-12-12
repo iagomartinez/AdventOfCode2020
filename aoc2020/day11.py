93798c9eb4414a6f44478b224aec0e66eb553a3c
Iago Martínez <iago@fundapps.co>
2020-12-12 18:25:16 +0000

    Day 11 Part 1 done: count occupied seats when grid stabilises

--- aoc2020/day11.py
+++ aoc2020/day11.py
@@ -1,5 +1,6 @@
 import sys
 import unittest
+unittest.TestLoader.sortTestMethodsUsing = None
 
 class Tests(unittest.TestCase):
     def test_parsegrid(self):
@@ -28,12 +29,64 @@ class Tests(unittest.TestCase):
         self.assertEqual([True, None, False, False, None, False, True, None, True, True], newgrid[0])
         self.assertEqual([True, False, False, False, False, False, False, None, False, True], newgrid[1])
 
+    def test_gridchanges_afterroundthree(self):
+        grid = parsegrid('../_data/day11_smallgrid.txt')
+        newgrid = applyrules(grid)
+        newgrid2 = applyrules(newgrid)
+        newgrid3 = applyrules(newgrid2)
+        print('---- after 3 rounds -----')
+        prettyprint(newgrid3)
+        print('-------------------------')
+        self.assertNotEqual(newgrid2, newgrid3)
+
+    def test_seatsfree_whengridstabilises(self):
+        grid = parsegrid('../_data/day11_smallgrid.txt')
+        stablegrid = parsegrid('../_data/day11_stablegrid.txt')
+        finalgrid = repeatuntilstable(grid, verbose=True)
+        self.assertTrue(finalgrid, stablegrid)
+        self.assertEqual(37, countoccupied(finalgrid))
+
+    def test_comparegrids(self):
+        grid = parsegrid('../_data/day11_smallgrid.txt')
+        newgrid = applyrules(grid)
+        clone = list([list(row) for row in grid])
+
+        self.assertFalse(equalgrids(grid, newgrid))
+        self.assertTrue(equalgrids(grid, clone))
+
+def countoccupied(grid):
+    return len([seat for row in grid for seat in row if occupied(seat)])
+
+def equalgrids(grid1, grid2):
+    return all([s0 == s1 for r0,r1 in zip(grid1, grid2) for s0,s1 in zip (r0, r1)])
+
 #   free + occupied are trivial, just for readability
 def free(seat):
     return seat == False
 
 def occupied(seat):
     return seat == True
+
+class MaxAttemptsExceededError(Exception):
+    pass
+
+def repeatuntilstable(grid, maxattempts=100, verbose=False):
+    rounds = 0
+    startgrid = list([list(row) for row in grid])
+    while True and rounds < maxattempts:
+        newgrid = applyrules(startgrid)
+        rounds += 1
+        if verbose:
+            print(f'---- after round {rounds} -----')
+            prettyprint(newgrid)
+            print('-------------------------')
+        if equalgrids(startgrid, newgrid):
+            if verbose:
+                print(f'Grids stable after {rounds} rounds')
+            return newgrid
+        else:
+            startgrid = list([list(row) for row in newgrid])
+    raise MaxAttemptsExceededError()
 
 def applyrules(grid, verbose=False):
     newgrid = list([list(row) for row in grid])
@@ -75,6 +128,9 @@ def parsegrid(file):
     
 def main():
     print('---------- Day 11 ----------')
+    startgrid = parsegrid('../_data/day11.txt')
+    stablegrid = repeatuntilstable(startgrid, verbose=True)
+    print(f'Stable grid found, occupied seats: {countoccupied(stablegrid)}')
 
 if __name__ == '__main__':
     sys.exit(main())
