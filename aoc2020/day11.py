efb02aa2ad99e58101449a48c822b72b8129838e
Iago Martínez <iago@fundapps.co>
2020-12-14 09:02:43 +0000

    Tried to speed up by caching visible seats, but no effect. Time to move on!

--- aoc2020/day11.py
+++ aoc2020/day11.py
@@ -1,6 +1,7 @@
 import sys
 import unittest
 import time
+import functools
 unittest.TestLoader.sortTestMethodsUsing = None
 
 class Tests(unittest.TestCase):
@@ -13,31 +14,21 @@ class Tests(unittest.TestCase):
     def test_seatsfree_whengridstabilises(self):
         grid = parsegrid('../_data/day11_smallgrid.txt')
         stablegrid = parsegrid('../_data/day11_stablegrid.txt')
-        finalgrid = repeatuntilstable(grid, verbose=True)
+        finalgrid,_ = repeatuntilstable(grid, verbose=True)
         self.assertTrue(finalgrid, stablegrid)
         self.assertEqual(26, countoccupied(finalgrid))
 
     def test_comparegrids(self):
         grid = parsegrid('../_data/day11_smallgrid.txt')
-        newgrid = applyrules(grid, True)
+        scanner = VisibleSeatsScanner(grid)
+        newgrid = applyrules(grid, scanner, True)
         clone = list([list(row) for row in grid])
         self.assertFalse(equalgrids(grid, newgrid))
         self.assertTrue(equalgrids(grid, clone))
 
-    def test_scandiagonal(self):
-        grid = parsegrid('../_data/day11_los.txt')
-        seatpos = scandiagonal(grid, 4,3,1,1)
-        self.assertEqual((5,4), seatpos)
-        seatpos = scandiagonal(grid, 4,3,-1,1)
-        self.assertEqual((0,7), seatpos)
-        seatpos = scandiagonal(grid, 4,3,1,-1)
-        self.assertEqual((7,0), seatpos)
-        seatpos = scandiagonal(grid, 4, 3,-1,-1)
-        self.assertEqual((2,1), seatpos)
-
     def test_lineofsight_onsmallgrid(self):
-        grid = parsegrid('../_data/day11_smallgrid.txt')
-        seen = checklineofsight(0, 9, grid, True)
+        scanner = VisibleSeatsScanner(parsegrid('../_data/day11_smallgrid.txt'))
+        seen = scanner.scan(0, 9)
         print(seen)
         self.assertEqual(3, len(seen))        
 
@@ -60,27 +51,26 @@ class MaxAttemptsExceededError(Exception):
 def repeatuntilstable(grid, maxattempts=100, verbose=False):
     rounds = 0
     startgrid = list([list(row) for row in grid])
+    scanner = VisibleSeatsScanner(grid)
     while True and rounds < maxattempts:
-        newgrid = applyrules(startgrid)
+        newgrid = applyrules(startgrid, scanner)
         rounds += 1
         if verbose:
             print(f'---- after round {rounds} -----')
             prettyprint(newgrid)
             print('-------------------------')
         if equalgrids(startgrid, newgrid):
-            if verbose:
-                print(f'Grids stable after {rounds} rounds')
-            return newgrid
+            return (newgrid, rounds)
         else:
             startgrid = list([list(row) for row in newgrid])
     raise MaxAttemptsExceededError()
 
-def applyrules(grid, verbose=False):
+def applyrules(grid, scanner, verbose=False):
     newgrid = list([list(row) for row in grid])
     for iy, seats in enumerate(grid):
         for ix, seat in enumerate(seats):
             if seat is not None:
-                visibleseats = [grid[y][x] for y,x in checklineofsight(iy, ix, grid)]
+                visibleseats = [grid[y][x] for (y,x) in scanner.scan(iy, ix)]
                 if verbose:
                     print(f'Scanning, {iy},{ix}, visible: {visibleseats}')
                 if (free(seat) and not any([occupied(seat) for seat in visibleseats])):
@@ -93,40 +83,46 @@ def applyrules(grid, verbose=False):
                     newgrid[iy][ix] = False
     return newgrid
 
-def scanrow(grid, startx, endx, row, step=1):
-    if startx in range(0, len(grid[row])):
-        for x in range(startx, endx, step):
-            if grid[row][x] is not None:
-                return (row, x)
-    return None
+class VisibleSeatsScanner():
+    def __init__(self, grid, verbose=False):
+        self.grid = grid
+        self.v = verbose
 
-def scancol(grid, starty, endy, col, step=1):
-    if starty in range(0, len(grid)):
-        for y in range(starty, endy, step):
-            if grid[y][col] is not None:
-                return (y, col)
-    return None
+    def __row(self, startx, endx, row, step=1):
+        if startx in range(0, len(self.grid[row])):
+            for x in range(startx, endx, step):
+                if self.grid[row][x] is not None:
+                    return (row, x)
+        return None
 
-def scandiagonal(grid, starty, startx, ystep, xstep, verbose=False):
-    ypos, xpos = starty + ystep, startx + xstep
-    while xpos in range(0, len(grid[starty]))  and ypos in range(0, len(grid)):
-        if grid[ypos][xpos] is not None:
-            return (ypos, xpos)
-        ypos += ystep
-        xpos += xstep
-    return None
+    def __col(self, starty, endy, col, step=1):
+        if starty in range(0, len(self.grid)):
+            for y in range(starty, endy, step):
+                if self.grid[y][col] is not None:
+                    return (y, col)
+        return None
 
-def checklineofsight(row, col, grid, verbose=False):
-    seen = set()
-    seen.add(scanrow(grid,col-1,-1,row,step=-1))
-    seen.add(scanrow(grid,col+1,len(grid[row]),row))
-    seen.add(scancol(grid,row-1,-1,col,step=-1))
-    seen.add(scancol(grid,row+1,len(grid),col))
-    seen.add(scandiagonal(grid,row,col,1,1, verbose))
-    seen.add(scandiagonal(grid,row,col,-1,1, verbose))
-    seen.add(scandiagonal(grid,row,col,1,-1, verbose))
-    seen.add(scandiagonal(grid,row,col,-1,-1, verbose))
-    return {pos for pos in seen if pos is not None}
+    def __diagonal(self, starty, startx, ystep, xstep):
+        ypos, xpos = starty + ystep, startx + xstep
+        while xpos in range(0, len(self.grid[starty]))  and ypos in range(0, len(self.grid)):
+            if self.grid[ypos][xpos] is not None:
+                return (ypos, xpos)
+            ypos += ystep
+            xpos += xstep
+        return None
+
+    @functools.lru_cache(maxsize=1000)
+    def scan(self, ypos, xpos):
+        seen = set()
+        seen.add(self.__row(xpos-1,-1,ypos,step=-1))
+        seen.add(self.__row(xpos+1,len(self.grid[ypos]),ypos))
+        seen.add(self.__col(ypos-1,-1,xpos,step=-1))
+        seen.add(self.__col(ypos+1,len(self.grid),xpos))
+        seen.add(self.__diagonal(ypos,xpos,1,1))
+        seen.add(self.__diagonal(ypos,xpos,-1,1))
+        seen.add(self.__diagonal(ypos,xpos,1,-1))
+        seen.add(self.__diagonal(ypos,xpos,-1,-1))
+        return {pos for pos in seen if pos is not None}
 
 def checkadjacent(iy, ix, grid):
     validrows = range(0, len(grid))
@@ -155,9 +151,9 @@ def main():
     print('---------- Day 11 ----------')
     startgrid = parsegrid('../_data/day11.txt')
     t0 = time.perf_counter()
-    stablegrid = repeatuntilstable(startgrid)
+    stablegrid, rounds = repeatuntilstable(startgrid)
     t1 = time.perf_counter()
-    print(f'Stable grid found, occupied seats: {countoccupied(stablegrid)} in {t1 - t0:0.4f} seconds')
+    print(f'Stable grid found after {rounds} rounds, occupied seats: {countoccupied(stablegrid)} in {t1 - t0:0.4f} seconds')
 
 if __name__ == '__main__':
     sys.exit(main())
