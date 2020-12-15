a66ce8c35fc3e2c42f7851aa97e5dba183230fda
Iago Martínez <iago@fundapps.co>
2020-12-15 10:18:19 +0000

    Day 12 Part 1 Done on second attempt - copy paste error -> wrong answer :facepalm:

--- aoc2020/day12.py
+++ aoc2020/day12.py
@@ -9,19 +9,19 @@ class Tests(unittest.TestCase):
         self.assertEqual(('F', 10), (action,value))       
 
     @parameterized.expand([
-        ('F10', (0, 10, 270), 10),
-        ('N10', (10, 0, 270), 10),
-        ('S10', (-10, 0, 270), 10),
-        ('E10', (0, 10, 270), 10),
-        ('W10', (0, -10, 270), 10),
-        ('R90', (0, 0, 0), 0),
-        ('R180', (0, 0, 90), 0),
-        ('R270', (0, 0, 180), 0),
-        ('R360', (0, 0, 270), 0),
-        ('L90', (0, 0, 180), 0),
-        ('L180', (0, 0, 90), 0),
-        ('L270', (0, 0, 0), 0),
-        ('L360', (0, 0, 270), 0)
+        ('F10', (0, 10, 90), 10),
+        ('N10', (10, 0, 90), 10),
+        ('S10', (-10, 0, 90), 10),
+        ('E10', (0, 10, 90), 10),
+        ('W10', (0, -10, 90), 10),
+        ('R90', (0, 0, 180), 0),
+        ('R180', (0, 0, 270), 0),
+        ('R270', (0, 0, 0), 0),
+        ('R360', (0, 0, 90), 0),
+        ('L90', (0, 0, 0), 0),
+        ('L180', (0, 0, 270), 0),
+        ('L270', (0, 0,180), 0),
+        ('L360', (0, 0, 90), 0)
     ])
     def test_navigation(self, instruction,expectedposition, expecteddistance):
         action, value = parse(instruction)
@@ -30,11 +30,17 @@ class Tests(unittest.TestCase):
         self.assertEqual(expectedposition, endpos)
         self.assertEqual(expecteddistance, navigator.manhattandistance())
 
+    def test_sample(self):
+        file = '../_data/day12_sample.txt'
+        _, distance = sail(file)
+        self.assertEqual(25, distance)
+
 class Navigator():
     def __init__(self, verbose=False):
-        self.position = (0, 0, 270)
+        self.position = (0, 0, 90)
         self.v = verbose
         self.log = [self.position]
+        self.directionmap = {0:1, 90:1, 180:-1, 270:-1}
 
     def move(self, action, value):
         latitude,longitude,direction = self.position
@@ -42,14 +48,17 @@ class Navigator():
             direction = ((direction - value) if action == 'L' else (direction + value)) % 360
             self.position = latitude, longitude, direction
         if action == 'F':
-            if direction == 270:
-                self.position = latitude, longitude + value, direction
-                self.log.append(self.position)
+            distance = value * self.directionmap[direction]
+            if direction in {90, 270}:
+                self.position = latitude, longitude + distance, direction
+            if direction in {0, 180}:
+                self.position = latitude + distance, longitude, direction                
+            self.log.append(self.position)
         if action in {'N', 'S'}:
             self.position =  (latitude - value) if action == 'S' else (latitude + value), longitude, direction 
             self.log.append(self.position)
         if action in {'E', 'W'}:
-            self.position = latitude, (longitude - value) if action == 'W' else (latitude + value), direction
+            self.position = latitude, (longitude - value) if action == 'W' else (longitude + value), direction
             self.log.append(self.position)
         return self.position
         
@@ -57,6 +66,15 @@ class Navigator():
         lat0,long0,_ = self.log[0]
         lat1,long1,_ = self.log[-1]
         return (abs(lat1-lat0) + abs(long1-long0))
+
+def sail(commands):
+    with open(commands, 'r', newline='', encoding='utf-8') as f:
+        instructions = [parse(line.strip()) for line in f]
+    nav = Navigator()
+    for action,value in instructions:
+        newpos = nav.move(action, value)
+        print(f'{action},{value} -> {newpos}, distance: {nav.manhattandistance()}')
+    return newpos, nav.manhattandistance()
 
 def parse(instruction):
     r = re.compile('(?P<action>(N|S|E|W|L|R|F))(?P<value>\d{1,})') 
@@ -66,6 +84,8 @@ def parse(instruction):
 
 def main():
     print('----------- day12 -----------')
+    finalpos, manhattandistance = sail('../_data/day12.txt')
+    print(f'final position: {finalpos}, Manhattan distance: {manhattandistance}')
 
 if __name__ == '__main__':
     sys.exit(main())
