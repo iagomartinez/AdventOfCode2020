6cfea3d2c95504b9d56bc08d03ed325fbef6e010
Iago Martínez <iago@fundapps.co>
2020-12-17 18:44:12 +0000

    Day 12 Part 2 done (finally). Would have been quicker to just re-write rotation from scratch!

--- aoc2020/day12.py
+++ aoc2020/day12.py
@@ -9,80 +9,127 @@ class Tests(unittest.TestCase):
         self.assertEqual(('F', 10), (action,value))
 
     @parameterized.expand([
-        ('F10', (0, 10, 90), 10),
-        # ('N10', (10, 0, 90), 10),
-        # ('S10', (-10, 0, 90), 10),
-        # ('E10', (0, 10, 90), 10),
-        # ('W10', (0, -10, 90), 10),
-        # ('R90', (0, 0, 180), 0),
-        # ('R180', (0, 0, 270), 0),
-        # ('R270', (0, 0, 0), 0),
-        # ('R360', (0, 0, 90), 0),
-        # ('L90', (0, 0, 0), 0),
-        # ('L180', (0, 0, 270), 0),
-        # ('L270', (0, 0,180), 0),
-        # ('L360', (0, 0, 90), 0)
+        ('F10', (10, 100), (11, 110)),
+        ('N3', (0, 0), (4, 10)),
+        ('S3', (0, 0), (-2, 10)),
+        ('E3', (0, 0), (1, 13)),
+        ('W3', (0, 0), (1, 7)),
+        ('R90', (0, 0), (-10,1)),
+        ('L90', (0, 0), (10, -1)),
+        ('R270', (0, 0), (10, -1)),
+        ('L270', (0, 0), (-10, 1)),
+        ('R360', (0, 0), (1, 10)),
+        ('L360', (0, 0), (1, 10)),
+        ('L180', (0, 0), (-1, -10)),
+        ('R180', (0, 0), (-1, -10)),
     ])
-    def test_navigation(self, instruction,expectedposition, expecteddistance):
+    def test_navigation(self, instruction,expectedposition, expectedwaypoint):
         action, value = parse(instruction)
-        navigator = Navigator()
+        navigator = Navigator(True)
         endpos = navigator.move(action, value)
         self.assertEqual(expectedposition, endpos)
-        self.assertEqual(expecteddistance, navigator.manhattandistance())
+        self.assertEqual(navigator.waypoint.position(), expectedwaypoint)
 
     def test_sample(self):
         file = '../_data/day12_sample.txt'
         _, distance = sail(file)
-        self.assertEqual(25, distance)
+        self.assertEqual(286, distance)
+
+class WayPoint():
+    def __init__(self, latitude, longitude, verbose=False):
+        self.relativelat = 1
+        self.relativelong = 10
+        self.setrelativeto(latitude, longitude)
+        self.directionmap = {0:(1,-1), 90:(1,1), 180:(-1,1), 270:(-1,-1)}
+        self.v = verbose
+
+    def position(self):
+        return (self.absolutelat, self.absolutelong)
+
+    def rotate(self, rotation, latitude, longitude):        
+        steps = abs(rotation) // 90
+        rotationstep = rotation // steps
+
+        rotatedlat = self.relativelat
+        rotatedlong = self.relativelong
+        for _ in range(steps):
+            # switch lat and long
+            if rotation > 0:
+                tmplat = rotatedlong * -1
+                tmplong = rotatedlat
+            else:
+                tmplat = rotatedlong
+                tmplong = rotatedlat * -1
+            rotatedlat, rotatedlong = tmplat, tmplong
+            if self.v:
+                print(f'in rotate: {rotatedlat}, {rotatedlong}')
+
+        if self.v:
+            print(f'{self.relativelat}, {self.relativelong} -> rotate {rotation} ->  {rotatedlat}, {rotatedlong}')
+        self.relativelat = rotatedlat
+        self.relativelong = rotatedlong
+        self.setrelativeto(latitude, longitude)
+
+    def movelat(self, units):
+        self.relativelat += units
+        self.absolutelat += units
+
+    def movelong(self, units):
+        self.relativelong += units
+        self.absolutelong += units
+
+    def plotrelativeto(self, startlatitude, startlongitude, units):
+        newlat, newlong = startlatitude + units * self.relativelat, startlongitude + units * self.relativelong
+        if self.v:
+            print(f'{startlatitude} + {units * self.relativelat}, {startlongitude} + {units * self.relativelong} ->  {newlat}, {newlong}')
+        self.setrelativeto(newlat, newlong)
+        return(newlat, newlong)
+
+    def setrelativeto(self, latitude, longitude):
+        self.absolutelat = latitude + self.relativelat
+        self.absolutelong = longitude + self.relativelong
 
 class Navigator():
     def __init__(self, verbose=False):
-        self.bearing = 90
         self.latitude = 0
         self.longitude = 0
         self.v = verbose
-        self.log = [(self.latitude, self.longitude, self.bearing)]
-        self.directionmap = {0:1, 90:1, 180:-1, 270:-1}
+        self.waypoint = WayPoint(self.latitude, self.longitude, verbose=verbose)
+        self.log = [(self.latitude, self.longitude)]
 
     def __turn(self, rotation):
-        self.bearing = (self.bearing + rotation) % 360
+        self.waypoint.rotate(rotation, self.latitude, self.longitude)
 
-    def __forward(self, distance):
-        if self.bearing in {90, 270}:
-            self.longitude += distance
-        if self.bearing in {0, 180}:
-            self.latitude += distance
-        self.log.append((self.latitude, self.longitude, self.bearing))
-
-    def __move(self, direction, distance):
-        if direction in {'N', 'S'}:
-            self.latitude  += distance
-        if direction in {'E', 'W'}:
-            self.longitude += distance
-        self.log.append((self.latitude, self.longitude, self.bearing))
+    def __forward(self, units):
+        newlat, newlong = self.waypoint.plotrelativeto(self.latitude, self.longitude, units)
+        self.latitude, self.longitude = newlat, newlong
+        self.log.append((self.latitude, self.longitude))
 
     def move(self, action, value):
         if action in {'R', 'L'}:
             rotation = -value if action == 'L' else value
             self.__turn(rotation)
         if action == 'F':
-            self.__forward(value * self.directionmap[self.bearing])
-        if action in {'N', 'S', 'E', 'W'}:
-            self.__move(action, -value if action in {'S', 'W'} else value)
-        return self.latitude, self.longitude, self.bearing
+            self.__forward(value)
+        if action in {'N', 'S'}:
+            self.waypoint.movelat(-value if action == 'S' else value)
+        if action in {'E', 'W'}:
+            self.waypoint.movelong(-value if action == 'W' else value)
+        return self.latitude, self.longitude
 
     def manhattandistance(self):
-        lat0,long0,_ = self.log[0]
-        lat1,long1,_ = self.log[-1]
+        lat0,long0 = self.log[0]
+        lat1,long1 = self.log[-1]
         return (abs(lat1-lat0) + abs(long1-long0))
 
-def sail(commands):
+def sail(commands, verbose=False):
     with open(commands, 'r', newline='', encoding='utf-8') as f:
         instructions = [parse(line.strip()) for line in f]
-    nav = Navigator()
+    nav = Navigator(verbose=verbose)
     for action,value in instructions:
         newpos = nav.move(action, value)
-        print(f'{action},{value} -> {newpos}, distance: {nav.manhattandistance()}')
+        if verbose:
+            print(f'{action},{value} -> {newpos}, waypoint: ({nav.waypoint.relativelat}, {nav.waypoint.relativelong}), distance: {nav.manhattandistance()}')
     return newpos, nav.manhattandistance()
 
 def parse(instruction):
