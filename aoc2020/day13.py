6556b7ae682541b25162ea2e05058ce495cb6cbe
Iago Martínez <iago@fundapps.co>
2020-12-19 11:54:32 +0000

    Tidy up code before part 2

--- aoc2020/day13.py
+++ aoc2020/day13.py
@@ -2,28 +2,35 @@ import sys
 import unittest
 from datetime import datetime
 
-def extractminutes(tup):
-    _, times = tup
-    return times % 1
+def extractwholeminutes(tup):
+    _, minutes = tup
+    return minutes % 1
 
-def main():
-    print('----------- day13 -----------')
-    file='../_data/day13.txt'
-    with open(file, 'r', newline='', encoding='utf-8') as f:
-        timestamp = int(f.readline().strip())
-        busids = f.readline().strip()
-
-    schedule = [int(id) for id in busids.split(',') if id != 'x']
-
+def part1(schedule, timestamp):
     # for each bus, find out how many times they have run since time 0
     timesrun = [(id, timestamp / id) for id in schedule]
     # order by greatest partial run (i.e. next one to complete)
-    timesrun.sort(key=extractminutes, reverse=True)
+    timesrun.sort(key=extractwholeminutes, reverse=True)
+    print(timesrun)
     id, runs =timesrun[0]
     # find out the last timestamp that bus ran
     lastrun = timestamp - id * int(runs)
     # et voilá:
-    print(f'Next bus: {id}, result: {id * (id - lastrun)}')    
+    print(f'Next bus: {id}, result: {id * (id - lastrun)}')
+
+def parse(file):
+    with open(file, 'r', newline='', encoding='utf-8') as f:
+        timestamp = int(f.readline().strip())
+        busids = f.readline().strip().split(',')
+    return timestamp, busids
+
+def main():
+    print('----------- day13 -----------')
+    file='../_data/day13.txt'
+    timestamp, busids = parse(file)
+
+    schedule = [int(id) for id in busids if id != 'x']
+    part1(schedule, timestamp)
 
 if __name__ == '__main__':
     sys.exit(main())
