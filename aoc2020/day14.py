27c3970cd4576a55e95c6d51a803a6a503a9e82a
Iago Martínez <iago@fundapps.co>
2020-12-30 11:04:28 +0000

    Create ChipEmulator class to prepare for part 2

--- aoc2020/day14.py
+++ aoc2020/day14.py
@@ -5,56 +5,65 @@ import re
 class Tests(unittest.TestCase):
     def test_parsemask(self):        
         line = 'mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X'
-        mask = parsemask(line)
+        mask = ChipEmulator.parsemask(line)
         self.assertEqual({34:0,29:1}, mask)
 
         line = 'mem[8] = 11'
-        pos, value = parseinstruction(line)
+        pos, value = ChipEmulator.parseinstruction(line)
         self.assertEqual((8, 11), (pos, value)) 
             
         memory = dict()
-        memory[pos] = apply(mask, value)
+        memory[pos] = ChipEmulator.apply_v1(mask, value)
         
         self.assertEqual(73, memory[pos])
 
     def test_sample(self):
         file = '../_data/day14_sample.txt'
-        memory = initialise(file)
+        em = ChipEmulator()
+        memory = em.initialise_v1(file)
         self.assertEqual(165, sum(memory.values()))
 
-def initialise(file):
-    mask = dict()
-    memory = dict()
-    with open(file, 'r', newline='', encoding='utf-8') as f:
-        for line in f:
-            clean = line.strip()
-            if clean.startswith('mask'):
-                mask = parsemask(clean)
-            else:
-                pos, value = parseinstruction(clean)
-                memory[pos] = apply(mask, value)
-    return memory    
+class ChipEmulator():
+    def __init__(self):
+        self.memory = dict()
 
-def apply(mask, value):
-    tmp = list(f"{value:036b}")
-    for i, v in mask.items():
-        tmp[i] = str(v)
-    return int(''.join(tmp), 2)
+    @staticmethod
+    def parseinstruction(line):
+        r = re.compile('mem\[(?P<pos>\d{1,})\] = (?P<val>\d{1,})')
+        m = r.match(line)
+        pos,value = int(m['pos']), int(m['val'])
+        return pos, value
 
-def parseinstruction(line):
-    r = re.compile('mem\[(?P<pos>\d{1,})\] = (?P<val>\d{1,})')
-    m = r.match(line)
-    pos,value = int(m['pos']), int(m['val'])
-    return pos, value
+    @staticmethod
+    def parsemask(line):
+        parsed = line.replace('mask = ', '')
+        mask = dict([(ix,int(val)) for ix, val in enumerate(parsed) if val != 'X'])
+        return mask
 
-def parsemask(line):
-    parsed = line.replace('mask = ', '')
-    mask = dict([(ix,int(val)) for ix, val in enumerate(parsed) if val != 'X'])
-    return mask
+    @staticmethod
+    def apply_v1(mask, value):
+        tmp = list(f"{value:036b}")
+        for i, v in mask.items():
+            tmp[i] = str(v)
+        return int(''.join(tmp), 2)  
+
+    def initialise_v1(self, file):
+        mask = dict()
+        with open(file, 'r', newline='', encoding='utf-8') as f:
+            for line in f:
+                clean = line.strip()
+                if clean.startswith('mask'):
+                    mask = ChipEmulator.parsemask(clean)
+                else:
+                    pos, value = ChipEmulator.parseinstruction(clean)
+                    self.memory[pos] = ChipEmulator.apply_v1(mask, value)
+        return self.memory  
+
 
 def main():
     print('----------- day14 -----------')
-    memory = initialise('../_data/day14.txt')
+    em = ChipEmulator()
+    memory = em.initialise_v1('../_data/day14.txt')
     print(f'Sum of values in memory is {sum(memory.values())}')
 
 if __name__ == '__main__':
